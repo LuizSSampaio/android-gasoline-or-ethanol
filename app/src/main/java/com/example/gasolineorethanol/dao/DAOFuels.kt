e2719519dcee5e56b19635806b8ede93b9cdc323
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2023-07-21 17:35:27 -0300

    :rocket:style: Final form ui

---
+++ app/src/main/java/com/example/gasolineorethanol/dao/DAOFuels.kt
@@ -0,0 +1,11 @@
+package com.example.gasolineorethanol.dao
+
+import java.math.BigDecimal
+
+class DAOFuels {
+
+    companion object {
+        val gasolinePrice: BigDecimal = BigDecimal.ZERO
+        val ethanolPrice: BigDecimal = BigDecimal.ZERO
+    }
+}
