25c198b8fcf4d9b4fa90315e727fa895755f2b28
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2023-07-21 14:28:45 -0300

    :rocket:style: Initial form ui

---
+++ app/src/main/java/com/example/gasolineorethanol/ui/activity/GasolineOrEthanolFormActivity.kt
@@ -0,0 +1,12 @@
+package com.example.gasolineorethanol.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.gasolineorethanol.R
+
+class GasolineOrEthanolFormActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_gasoline_or_ethanol_form)
+    }
+}
